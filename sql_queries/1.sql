052e3958a7bdf416fac1578d9bf6b50617c5c4d2
Ifiokobong Akpan <iakpan916@gmail.com>
2022-02-12 00:13:43 +0100

    The average heights of male and female in Nigeria

--- sql_queries/1.sql
+++ sql_queries/1.sql
@@ -1,6 +1,10 @@
 -- Fetching all the data from the database
 select * from Heights..heights
 
--- The tallest male in the world
+-- The tallest male in the world in cm
 select max(male_height_cm) as "Tallest Male"
 from Heights..heights
+
+-- The average heights of male and female in Nigeria
+select * from Heights..heights
+where country_name = 'Nigeria'
