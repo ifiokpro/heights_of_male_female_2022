b817f1f98fc593504ee913b26739818ca9e4c9e6
Ifiokobong Akpan <iakpan916@gmail.com>
2022-02-13 15:45:36 +0100

    Updated countries queries

--- sql_queries/1.sql
+++ sql_queries/1.sql
@@ -1,8 +1,6 @@
 -- Fetching all the data from the database
 select * from Heights..heights
 
-
-
 -- The average heights of male and female in Nigeria
 select * from Heights..heights
 where country_name = 'Nigeria'
---
+++ sql_queries/country_name/country.sql
@@ -0,0 +1,3 @@
+-- Queries fetching country names that starts with letter c
+select country_name from Heights..heights
+where country_name like 'C%'
