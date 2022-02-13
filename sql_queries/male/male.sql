b083a8aeee941f320b181ef8f345358cdb902e7a
Ifiokobong Akpan <iakpan916@gmail.com>
2022-02-13 15:37:56 +0100

    Updated maximum height of male

--- sql_queries/male/male.sql
+++ sql_queries/male/male.sql
@@ -5,3 +5,7 @@ from Heights..heights
 -- Minimum average height of Males from all countries
 select (MIN(male_height_cm)) as MinimumMaleHeight 
 from Heights..heights
+
+-- Maximum average height of Males from all countries
+select (Max(female_height_cm)) as MinimumMaleHeight 
+from Heights..heights
