f3258c90a482be5fbf0d33f44f7bb5dfcc5fe147
Ifiokobong Akpan <iakpan916@gmail.com>
2022-02-13 15:39:40 +0100

    Updated the queries

--- sql_queries/male/male.sql
+++ sql_queries/male/male.sql
@@ -1,11 +1,11 @@
--- The tallest male in the world in cm
+-- The tallest male (in cm) in the world in cm
 select max(male_height_cm) as "Tallest Male"
 from Heights..heights
 
--- Minimum average height of Males from all countries
+-- Minimum average height (in cm) of Males from all countries
 select (MIN(male_height_cm)) as MinimumMaleHeight 
 from Heights..heights
 
--- Maximum average height of Males from all countries
+-- Maximum average height (in cm) of Males from all countries
 select (Max(female_height_cm)) as MinimumMaleHeight 
 from Heights..heights
